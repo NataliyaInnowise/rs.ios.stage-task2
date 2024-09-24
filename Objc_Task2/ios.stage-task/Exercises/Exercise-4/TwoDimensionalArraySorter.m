d47aa4c1f4dc1babe0105e8b3120c70b3b2ce674
Natalia Lazovskaya <natalia.lazovskaya@innowise.com>
2024-09-24 13:00:00 +0200

    add TwoDimensionalArraySorter solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,53 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+        if (![self isTwoDimensionalArray:array]) {
+            return @[];
+        }
+        
+        NSMutableArray *numbersArray = [NSMutableArray array];
+        NSMutableArray *stringArray = [NSMutableArray array];
+        
+        for (NSArray *subArray in array) {
+            for (id element in subArray) {
+                if ([element isKindOfClass:[NSNumber class]]) {
+                    [numbersArray addObject:element];
+                } else if ([element isKindOfClass:[NSString class]]) {
+                    [stringArray addObject:element];
+                }
+            }
+        }
+        
+        if ([numbersArray count] > 0 && [stringArray count] == 0) {
+            [numbersArray sortUsingSelector:@selector(compare:)];
+            return numbersArray;
+        }
+        
+        if ([stringArray count] > 0 && [numbersArray count] == 0) {
+            // Case 2: Only strings
+            [stringArray sortUsingSelector:@selector(compare:)];
+            return stringArray;
+        }
+        
+        if ([numbersArray count] > 0 && [stringArray count] > 0) {
+            
+            [numbersArray sortUsingSelector:@selector(compare:)];
+            NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil ascending:NO];
+            NSArray *sortedStrings = [stringArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+
+            return @[numbersArray, sortedStrings];
+        }
+        
+        return @[];
+}
+
+- (BOOL)isTwoDimensionalArray:(NSArray *)array {
+    for (id element in array) {
+        if (![element isKindOfClass:[NSArray class]]) {
+            return NO;
+        }
+    }
+    return YES;
 }
 
 @end
